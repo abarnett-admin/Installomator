4922091259c28d846f65c6d8f9508c40df12bedd
Andrew Barnett <138056529+AndrewMBarnett@users.noreply.github.com>
2024-12-06 11:25:28 -0600

    Updated Installomator Custom
    Updated citrixworkspace

--- InstallomatorCustom.sh
+++ InstallomatorCustom.sh
@@ -338,8 +338,8 @@ if [[ $(/usr/bin/arch) == "arm64" ]]; then
     fi
 fi
 
-VERSION="12.33"
-VERSIONDATE="2024-12-06"
+VERSION="12.34"
+VERSIONDATE="2024-12-07"
 
 # MARK: Functions
 
@@ -3011,21 +3011,22 @@ citrixworkspace)
     #credit: Erik Stam (@erikstam) and #Philipp on MacAdmins Slack
     name="Citrix Workspace"
     type="pkgInDmg"
+    curlOptions=( --user-agent "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36")
     parseURL() {
         urlToParse='https://www.citrix.com/downloads/workspace-app/mac/workspace-app-for-mac-latest.html#ctx-dl-eula-external'
-        htmlDocument=$(curl -s -L $urlToParse)
+        htmlDocument=$(curl -s -L $urlToParse $curlOptions)
         xmllint --html --xpath "string(//a[contains(@rel, 'downloads.citrix.com')]/@rel)" 2> /dev/null <(print $htmlDocument)
     }
     downloadURL="https:$(parseURL)"
     newVersionString() {
         urlToParse='https://www.citrix.com/downloads/workspace-app/mac/workspace-app-for-mac-latest.html'
-        htmlDocument=$(curl -fs $urlToParse)
-        xmllint --html --xpath 'string(//p[contains(., "Version")])' 2> /dev/null <(print $htmlDocument)
-    }
-    appNewVersion=$(newVersionString | cut -d ' ' -f2 )
-    versionKey="CitrixVersionString"
-    expectedTeamID="S272Y5R93J"
-    ;;
+         htmlDocument=$(curl -fs $urlToParse $curlOptions)
+         xmllint --html --xpath 'string(//p[contains(., "Version")])' 2> /dev/null <(print $htmlDocument)
+     }
+     appNewVersion=$(newVersionString | cut -d ' ' -f2 | cut -d '(' -f1)
+     versionKey="CitrixVersionString"
+     expectedTeamID="S272Y5R93J"
+     ;;
 cleartouchcollage)
     name="Collage"
     type="pkgInZip"
